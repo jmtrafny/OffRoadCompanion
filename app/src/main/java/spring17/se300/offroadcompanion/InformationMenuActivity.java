0a23bff797a347316c4a81dd69936510e9e311bf
WillJms <jamesw2@my.erau.edu>
2017-03-20 04:43:31 -0400

    Complete InformationMenuActivity and a Trial activity for the information pages.

--- app/src/main/java/spring17/se300/offroadcompanion/InformationMenuActivity.java
+++ app/src/main/java/spring17/se300/offroadcompanion/InformationMenuActivity.java
@@ -1,7 +1,9 @@
 package spring17.se300.offroadcompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by jmtra on 2/20/2017.
@@ -13,6 +15,13 @@ public class InformationMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_menu);
     }
+
+    public void trailButtonClicked(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        startActivity(intent);
+    }
+
 }
 //Patrick Added a comment per the push-pull homework.
 //Johny Done Goofed!!
