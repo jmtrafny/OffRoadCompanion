844e0b885b20b19d1464bf308376a939c37cbe86
lewise8 <lewise8@my.erau.edu>
2017-03-30 13:11:45 -0400

    Map is now working with Location services. In the Build.gradle file, the version must be changed to 21 for target and lowest SDK. Commenting to come

--- app/src/main/java/spring17/se300/offroadcompanion/NavigationActivity.java
+++ app/src/main/java/spring17/se300/offroadcompanion/NavigationActivity.java
@@ -1,21 +1,47 @@
 package spring17.se300.offroadcompanion;
 
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-
-import android.location.Location;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 /**
  * This shows how to use a custom location source.
  */
-public class NavigationActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class NavigationActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks {
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
 
     /**
      * A {@link LocationSource} which reports a new location whenever a user long presses the map
@@ -66,6 +92,9 @@ public class NavigationActivity extends AppCompatActivity implements OnMapReadyC
 
     private LongPressLocationSource mLocationSource;
 
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,6 +102,12 @@ public class NavigationActivity extends AppCompatActivity implements OnMapReadyC
 
         mLocationSource = new LongPressLocationSource();
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -92,9 +127,21 @@ public class NavigationActivity extends AppCompatActivity implements OnMapReadyC
 
     @Override
     public void onMapReady(GoogleMap map) {
+        mMap = map;
+        mMap.setOnMapLongClickListener(mLocationSource);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(29.189329, -81.048740)));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    }
 
-        map.setLocationSource(mLocationSource);
-        map.setOnMapLongClickListener(mLocationSource);
-        map.setMyLocationEnabled(true);
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 }
