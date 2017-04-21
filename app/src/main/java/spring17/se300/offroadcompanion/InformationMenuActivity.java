dd6dd06916c06012480d1db98cabca33db0b0556
WillJms <jamesw2@my.erau.edu>
2017-04-21 00:43:10 -0400

    Fixed a quick formatting error

--- app/src/main/java/spring17/se300/offroadcompanion/InformationMenuActivity.java
+++ app/src/main/java/spring17/se300/offroadcompanion/InformationMenuActivity.java
@@ -19,10 +19,106 @@ public class InformationMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_information_menu);
     }
 
-    public void trailButtonClicked(View view) {
+    public void etButtonClicked(View view) {
         // Do something in response to button
+        int pageId = 0;
         Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
         startActivity(intent);
     }
 
+    public void bdButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 1;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+
+    public void adButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 2;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+
+    public void mButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 3;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+
+    public void rcButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 4;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void siButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 5;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void wButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 6;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void usButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 7;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void fsButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 8;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void rorButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 9;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void wtpButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 10;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void tButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 11;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void bmrButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 12;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
+    public void tntcButtonClicked(View view) {
+        // Do something in response to button
+        int pageId = 13;
+        Intent intent = new Intent(InformationMenuActivity.this, InformationPageActivity.class);
+        intent.putExtra("page", pageId);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/spring17/se300/offroadcompanion/InformationPageActivity.java
+++ app/src/main/java/spring17/se300/offroadcompanion/InformationPageActivity.java
@@ -1,9 +1,14 @@
 package spring17.se300.offroadcompanion;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by William James on 3/19/2017.
@@ -13,9 +18,114 @@ import android.view.View;
 public class InformationPageActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int screen = getIntent().getExtras().getInt("page");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_information_page);
+        String text = "";
+        InputStream is;
+        TextView textElement;
+        switch (screen){
+            case 0:
+                setContentView(R.layout.activity_trailetiquette);
+                textElement = (TextView) findViewById(R.id.trailText);
+                is = getResources().openRawResource(R.raw.ettext);
+                text = readFile(is);
+                textElement.setText(text);
+
+                break;
+            case 1: setContentView(R.layout.activity_basicdriving);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 2: setContentView(R.layout.activity_advanceddriving);
+                textElement = (TextView) findViewById(R.id.adText);
+                is = getResources().openRawResource(R.raw.adtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 3: setContentView(R.layout.activity_muddin);
+                textElement = (TextView) findViewById(R.id.mudText);
+                is = getResources().openRawResource(R.raw.mudtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 4: setContentView(R.layout.activity_rock);
+                textElement = (TextView) findViewById(R.id.rText);
+                is = getResources().openRawResource(R.raw.rocktext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 5: setContentView(R.layout.activity_snow);
+                textElement = (TextView) findViewById(R.id.snowText);
+                is = getResources().openRawResource(R.raw.snowtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 6: setContentView(R.layout.activity_whinching);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 7: setContentView(R.layout.activity_you);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 8: setContentView(R.layout.activity_friend);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 9: setContentView(R.layout.activity_responsibility);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 10: setContentView(R.layout.activity_pack);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 11: setContentView(R.layout.activity_tools);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 12: setContentView(R.layout.activity_fix);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+            case 13: setContentView(R.layout.activity_tips);
+                textElement = (TextView) findViewById(R.id.basicText);
+                is = getResources().openRawResource(R.raw.bdtext);
+                text = readFile(is);
+                textElement.setText(text);
+                break;
+
+        }
     }
 
-
+    protected String readFile(InputStream io){
+        BufferedReader br = new BufferedReader(new InputStreamReader(io));
+        String line;
+        String entireFile = "";
+        try {
+            while((line = br.readLine()) != null) {
+                entireFile += (line + "\n");
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return entireFile;
+    }
 }
